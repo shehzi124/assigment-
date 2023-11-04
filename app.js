3d8e44f75b540482be564f6b59b25b6700399215
shehzi124 <shahzad2539184@gmail.com>
2023-11-04 12:14:41 +0500

    assigment done

---
+++ app.js
@@ -0,0 +1,50 @@
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+
+
+// for (let i = 1; i <+ 10; i++){
+//     console.log(i);
+// }
+
+
+
+// for (let i = 1; i <= 10; i++){
+//     console.log("5 x " + i + " = " + 5 * i);
+// }
+
+
+// const div = document.querySelector('.container')
+
+// let userVal = +prompt('enter your value');
+// for (let i = 1; i <= 10; i++){
+//     console.log(userVal + " x " + i + " = " + userVal * i);
+//     div.innerHTML += userVal + " x " + i + " = "  + userVal * i + "<br/>";
+// }
+
+
+
+// document.getElementById("multiplication-form").addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     // Get the input number
+//     var number = parseInt(document.getElementById("number").value);
+    
+//     // Generate the multiplication table
+//     var tableHtml = "<h2>Multiplication Table for " + number + "</h2><table>";
+//     for (var i = 1; i <= 10; i++) {
+//         tableHtml += "<tr><td>" + number + " x " + i + "</td><td>=</td><td>" + (number * i) + "</td></tr>";
+//     }
+//     tableHtml += "</table>";
+
+//     // Display the table
+//     document.getElementById("table-container").innerHTML = tableHtml;
+// });
+
+
+
+
+
+
+
+
+
